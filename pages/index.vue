d6873c528cb4f2c3ba80d22abb12630661085d0c
nakabayashi kotaro <123windam@gmail.com>
2022-03-29 10:59:15 +0900

    データ追加

--- pages/index.vue
+++ pages/index.vue
@@ -45,6 +45,7 @@ import FullCalendar from '@fullcalendar/vue'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
+
 import { formatDate } from '@fullcalendar/vue';
 
 let eventGuid = 0
@@ -52,16 +53,56 @@ let todayStr = new Date().toISOString().replace(/T.*$/, '') //
 
 const INITIAL_EVENTS = [
   {
-    id: createEventId(),
-    title: 'All-day event',
-    start: todayStr
+    title: 'Business Lunch',
+    start: '2022-03-03T13:00:00',
+    constraint: 'businessHours'
   },
   {
-    id: createEventId(),
-    title: 'Timed event',
-    start: todayStr + 'T12:00:00'
+    title: 'Meeting',
+    start: '2022-03-13T11:00:00',
+    constraint: 'availableForMeeting', // defined below
+    color: '#257e4a'
+  },
+  {
+    title: 'Conference',
+    start: '2022-03-18',
+    end: '2022-03-20'
+  },
+  {
+    title: 'Party',
+    start: '2022-03-29T20:00:00'
+  },
+
+  // areas where "Meeting" must be dropped
+  {
+    groupId: 'availableForMeeting',
+    start: '2022-03-11T10:00:00',
+    end: '2022-03-11T16:00:00',
+    rendering: 'background'
+  },
+  {
+    groupId: 'availableForMeeting',
+    start: '2022-03-13T10:00:00',
+    end: '2022-03-13T16:00:00',
+    rendering: 'background'
+  },
+
+  // red areas where no events can be dropped
+  {
+    start: '2022-03-24',
+    end: '2022-03-28',
+    overlap: false,
+    rendering: 'background',
+    color: '#ff9f89'
+  },
+  {
+    start: '2022-03-06',
+    end: '2022-03-08',
+    overlap: false,
+    rendering: 'background',
+    color: '#ff9f89'
   }
-]
+];
 
 function createEventId() {
   return String(eventGuid++)
@@ -94,7 +135,12 @@ export default {
         weekends: true,
         select: this.handleDateSelect,
         eventClick: this.handleEventClick,
-        eventsSet: this.handleEvents
+        eventsSet: this.handleEvents,
+        locale: 'ja',
+        timeZone: 'Asia/Tokyo',
+        buttonIcons: false,
+        eventTimeFormat: { hour: 'numeric', minute: '2-digit' },
+        defaultView: 'listMonth',
       },
 
 
